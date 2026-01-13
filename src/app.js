7beeb92392aaf73b8f1a709126ff9ed4e8965508
phophaliakhushdeep0291-ux <phophaliakhushdeep0291@gmail.com>
2026-01-13 21:43:56 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express"
-import cors from " cors"
+import cors from "cors"
 import cookieParser from "cookie-parser"
 
 const app= express()
@@ -13,6 +13,9 @@ app.use(express.urlencoded({extended: true,limit:"20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+import userRouter from './routes/user.routes.js'
 
+
+app.use("/api/v1/users",userRouter)
 
 export {app}
